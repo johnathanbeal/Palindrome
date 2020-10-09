9c3d8370597197da6155946a10c0a0cf9506ebb0
Johnathan Beal <johnathanbeal@gmail.com>
2020-10-08 20:31:07 -0400

    it took 30 minutes or less to write this, without Googling anything other than examples of Palindromes

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace Palindrome
+{
+    public class Program
+    {
+        public static void Main(string[] palindromes)
+        {
+            Console.WriteLine("Enter a palindrome");
+            
+            var input  = Console.ReadLine();
+            var entries = input.Split(' ');
+
+            var counter = 0;
+
+            foreach (var entry in entries)
+            {
+                var loops = entry.Length;
+                var doppleganger = "";
+
+                while (loops > 0)
+                {
+                    doppleganger = doppleganger + entry[loops - 1];
+                    loops = loops - 1;
+                }
+
+                counter++;
+
+                if (entry == doppleganger)
+                {
+                    Console.WriteLine(counter + " is a Palindrome");
+                }
+                else
+                {
+                    Console.WriteLine(counter + " is NOT a Palindrome");
+                }
+            }
+        }
+    }
+}
